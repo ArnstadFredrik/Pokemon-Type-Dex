f0c51c1cd7646e443dfbf0335057afa35ca3ae29
Fredrik Arnstad <fredrikarnstad@icloud.com>
2020-01-05 08:39:01 +0100

    sw

---
+++ docs/sw.js
@@ -0,0 +1,39 @@
+const v = '0.0.2';
+const cacheArray = [
+  './assets/css/main.css',
+  './sw.js',
+  './assets/css/script.js',
+
+];
+self.addEventListener('install', async event => {
+  const cache = await caches.open(`typedex`);
+  cache.addAll(cacheArray);
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if(url.origin == location.origin){
+    event.respondWith(cacheFirst(req));
+  } else {
+    event.respondWith(networkFirst(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cachedRespons = await caches.match(req);
+  return cachedRespons || fetch(req);
+}
+
+async function networkFirst(req) {
+  const cache = await caches.open('font');
+
+  try {
+    const res = await fetch(req);
+    caches.put(req, res.clone());
+    return res;
+  } catch (e) {
+    return await caches.match(req);
+  }
+}
